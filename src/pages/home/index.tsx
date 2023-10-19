6179279280e1c3efda1f3ea3b3962eaa14cd6280
nobytex <140118895+nobytex@users.noreply.github.com>
2023-10-19 11:34:03 -0300

    edição h1 de Sujeito programador para Meus links

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -69,7 +69,7 @@ export function Home(){
     
     return(
         <div className="flex flex-col w-full py-4 items-center justify-center">
-            <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Sujeito Programador</h1>
+            <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Meus links</h1>
             <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
             <main className="flex flex-col w-11/12 max-w-xl text-center">
